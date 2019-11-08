81efafeeefc74e57b0154445e7dbe3429cb21abb
ann850903 <ann850903@gmail.com>
2019-11-09 00:20:47 +0800

    change model, add decision tree and random forest, and plot precision_recall curve and ROC curve

--- hw2_r07943097.py
+++ hw2_r07943097.py
@@ -10,9 +10,13 @@ from keras.utils import to_categorical
 from keras.callbacks import EarlyStopping, CSVLogger
 #from keras.wrappers.scikit_learn import KerasClassifier
 #from sklearn.model_selection import GridSearchCV
-from sklearn.metrics import confusion_matrix, recall_score, precision_score, f1_score
+from sklearn import metrics
 from sklearn.utils.multiclass import unique_labels
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from inspect import signature
 
+input_node_num = 30
 lr = 0.01
 batch_size = 32
 epochs = 60
@@ -26,11 +30,11 @@ output_filename = 'r07943097_answer.txt'
 def load_data(file):
     df = pd.read_csv(file)
     if file == training_filename:
-        x_train = np.array(df.iloc[:,0:30].copy())
-        y_train = np.array(df.iloc[:,30].copy())
+        x_train = np.array(df.iloc[:,0:input_node_num].copy())
+        y_train = np.array(df.iloc[:,input_node_num].copy())
         return x_train,y_train
     else:
-        x_hidden = np.array(df.iloc[:,0:30].copy())
+        x_hidden = np.array(df.iloc[:,0:input_node_num].copy())
         return x_hidden
 
 def train_test_split(training_set, validation_split_ratio): 
@@ -41,9 +45,9 @@ def train_test_split(training_set, validation_split_ratio):
 
 def get_model():
     model = Sequential()
-    model.add(Dense(512, activation='relu', input_shape=(30,)))
+    model.add(Dense(512, activation='relu', input_shape=(input_node_num,)))
     model.add(Dropout(0.1))
-    model.add(Dense(512, activation='relu'))
+    model.add(Dense(1024, activation='relu'))
     model.add(Dropout(0.1))
     model.add(Dense(num_classes, activation='softmax'))
     optimizer = Adam(lr=lr)
@@ -68,16 +72,15 @@ def plot_loss_accuracy(file):
     accuracy, = plt.plot(accuracy)
     val_accuracy, = plt.plot(val_accuracy)
     plt.legend([accuracy, val_accuracy], ['accuracy', 'val_accuracy'], loc='lower right')
-    plt.show()
 
 def plot_confusion_matrix(y_true, y_pred, classes,
                           normalize=False,
                           title=None,
                           cmap=plt.cm.Blues):
-    """
+    '''
     This function prints and plots the confusion matrix.
     Normalization can be applied by setting `normalize=True`.
-    """
+    '''
     if not title:
         if normalize:
             title = 'Normalized confusion matrix'
@@ -85,7 +88,8 @@ def plot_confusion_matrix(y_true, y_pred, classes,
             title = 'Confusion matrix, without normalization'
 
     # Compute confusion matrix
-    cm = confusion_matrix(y_true, y_pred)
+    cm = metrics.confusion_matrix(y_true, y_pred)
+    
     # Only use the labels that appear in the data
     classes = classes[unique_labels(y_true, y_pred)]
     if normalize:
@@ -123,104 +127,217 @@ def plot_confusion_matrix(y_true, y_pred, classes,
     fig.tight_layout()
     return ax
 
-### load training file
-x_train, y_train = load_data(training_filename)
-y_train = to_categorical(y_train, num_classes)
-x_train, x_test = train_test_split(x_train, validation_split)
-y_train, y_test = train_test_split(y_train, validation_split)
-
-### grid search
-'''
-model = KerasClassifier(build_fn=get_model, epochs=epochs, batch_size=batch_size, verbose=0)
-param_grid = dict()
-grid = GridSearchCV(estimator=model, param_grid=param_grid, n_jobs=-1, cv=3)
-grid_result = grid.fit(x_train, y_train)
-# summarize results
-print("Best: %f using %s" % (grid_result.best_score_, grid_result.best_params_))
-means = grid_result.cv_results_['mean_test_score']
-stds = grid_result.cv_results_['std_test_score']
-params = grid_result.cv_results_['params']
-for mean, stdev, param in zip(means, stds, params):
-    print("%f (%f) with: %r" % (mean, stdev, param))
-'''
-
-if not os.path.exists('model_weight.h5'): 
-    ### build model
-    model = get_model()
-    earlyStopping = EarlyStopping(monitor='val_loss', patience=5, verbose=2)
-    csv_logger = CSVLogger('training.log')
-    record = model.fit(x_train, y_train,
-                        batch_size=batch_size,
-                        epochs=epochs,
-                        verbose=1,
-                        callbacks=[earlyStopping, csv_logger], 
-                        validation_data=(x_test, y_test))
-    ### plot loss and accuracy
+def plot_precision_recall_curve(y_true, y_pred): 
+    average_precision = metrics.average_precision_score(y_true, y_pred)
+    precision, recall, thresholds = metrics.precision_recall_curve(y_true, y_pred)
+    print('AUPRC:', metrics.auc(precision, recall))
+    
+    # In matplotlib < 1.5, plt.fill_between does not have a 'step' argument
+    step_kwargs = ({'step': 'post'}
+                   if 'step' in signature(plt.fill_between).parameters
+                   else {})
     plt.figure()
-    plt.plot(record.history['loss'],label='loss')
-    plt.plot(record.history['val_loss'],label='val_loss')
-    plt.legend(loc='upper right')
+    plt.step(recall, precision, color='b', alpha=0.2,
+             where='post')
+    plt.fill_between(recall, precision, alpha=0.2, color='b', **step_kwargs)
+    
+    plt.xlabel('Recall')
+    plt.ylabel('Precision')
+    plt.ylim([0.0, 1.05])
+    plt.xlim([0.0, 1.0])
+    plt.title('test Precision-Recall curve: AP={0:0.2f}'.format(
+              average_precision))
+
+def plot_ROC(y_true, y_pred): 
+    fpr, tpr, thresholds = metrics.roc_curve(y_true, y_pred)
+    print('AUROC:', metrics.auc(fpr, tpr))
     
     plt.figure()
-    plt.plot(record.history['accuracy'],label='acc')
-    plt.plot(record.history['val_accuracy'],label='val_acc')
-    plt.legend(loc='lower right')
+    plt.plot(fpr, tpr, color='darkorange', label='ROC curve')
+    plt.plot([0, 1], [0, 1], color='navy', linestyle='--')
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('Receiver operating characteristic')
+    plt.legend(loc="lower right")
+
+if __name__ == "__main__":
+    ### load training file
+    x_train, y_train = load_data(training_filename)
+    y_train = to_categorical(y_train, num_classes)
+    x_train, x_test = train_test_split(x_train, validation_split)
+    y_train, y_test = train_test_split(y_train, validation_split)
+    
+    ### grid search
+    '''
+    model = KerasClassifier(build_fn=get_model, epochs=epochs, batch_size=batch_size, verbose=0)
+    param_grid = dict()
+    grid = GridSearchCV(estimator=model, param_grid=param_grid, n_jobs=-1, cv=3)
+    grid_result = grid.fit(x_train, y_train)
+    # summarize results
+    print("Best: %f using %s" % (grid_result.best_score_, grid_result.best_params_))
+    means = grid_result.cv_results_['mean_test_score']
+    stds = grid_result.cv_results_['std_test_score']
+    params = grid_result.cv_results_['params']
+    for mean, stdev, param in zip(means, stds, params):
+        print("%f (%f) with: %r" % (mean, stdev, param))
+    '''
+    if not os.path.exists('model_weight.h5'): 
+        ### build model
+        model = get_model()
+        earlyStopping = EarlyStopping(monitor='val_loss', patience=10, verbose=2)
+        csv_logger = CSVLogger('training.log')
+        record = model.fit(x_train, y_train,
+                            batch_size=batch_size,
+                            epochs=epochs,
+                            verbose=1,
+                            callbacks=[earlyStopping, csv_logger], 
+                            validation_data=(x_test, y_test))
+        ### plot loss and accuracy
+        plt.figure()
+        plt.plot(record.history['loss'],label='loss')
+        plt.plot(record.history['val_loss'],label='val_loss')
+        plt.legend(loc='upper right')
+        
+        plt.figure()
+        plt.plot(record.history['accuracy'],label='acc')
+        plt.plot(record.history['val_accuracy'],label='val_acc')
+        plt.legend(loc='lower right')
+        plt.show()
+        model.save('model_weight.h5')
+    else:
+        model = load_model('model_weight.h5')
+        plot_loss_accuracy('training.log')
+    
+    
+    ### evaluate loss and accuracy
+    score = model.evaluate(x_train, y_train)
+    print('Train loss:', score[0])
+    print('Train accuracy:', score[1])
+    
+    score = model.evaluate(x_test, y_test)
+    print('Test loss:', score[0])
+    print('Test accuracy:', score[1])
+    
+    ### load hidden test set file
+    x_hidden = load_data(hidden_test_set_filename)
+    
+    ### predict hidden test set
+    y_pred_hidden = np.argmax(model.predict(x_hidden), axis=1)
+    
+    ### output my answer
+    with open(output_filename, 'w') as fo: 
+        for predict in y_pred_hidden:
+            fo.write(str(predict) + "\n")
+    
+    ### plot_confusion_matrix and calculate precision, recall, f1_score of model
+    #### training set
+    y_true = np.argmax(y_train, axis=1)
+    y_pred = np.argmax(model.predict(x_train), axis=1)
+    
+    print('Model:')
+    plot_confusion_matrix(y_true, y_pred, classes=np.arange(num_classes),
+                          title='train_confusion_matrix')
+    
+    train_precision = metrics.precision_score(y_true, y_pred)
+    print('train_precision:', train_precision)
+    train_recall = metrics.recall_score(y_true, y_pred)
+    print('train_recall:', train_recall)
+    train_f1_score = metrics.f1_score(y_true, y_pred)
+    print('train_f1_score:', train_f1_score)
+    
+    #### validation set
+    y_true = np.argmax(y_test, axis=1)
+    y_pred = np.argmax(model.predict(x_test), axis=1)
+    plot_confusion_matrix(y_true, y_pred, classes=np.arange(num_classes),
+                          title='validation_confusion_matrix')
+    
+    test_precision = metrics.precision_score(y_true, y_pred)
+    print('test_precision:', test_precision)
+    test_recall = metrics.recall_score(y_true, y_pred)
+    print('test_recall:', test_recall)
+    test_f1_score = metrics.f1_score(y_true, y_pred)
+    print('test_f1_score:', test_f1_score)
+    
+    plot_ROC(y_true, y_pred)
+    plot_precision_recall_curve(y_true, y_pred)
+    
+    #### average score
+    print('average_precision:', (train_precision+test_precision)/2)
+    print('average_recall:', (train_recall+test_recall)/2)
+    print('average_f1_score:', (train_f1_score+test_f1_score)/2)
     plt.show()
-    model.save('model_weight.h5')
-else:
-    model = load_model('model_weight.h5')
-    plot_loss_accuracy('training.log')
-
-### evaluate loss and accuracy
-score = model.evaluate(x_train, y_train)
-print('Train loss:', score[0])
-print('Train accuracy:', score[1])
-
-score = model.evaluate(x_test, y_test)
-print('Test loss:', score[0])
-print('Test accuracy:', score[1])
-
-### load hidden test set file
-x_hidden = load_data(hidden_test_set_filename)
-
-### predict hidden test set
-y_pred_hidden = np.argmax(model.predict(x_hidden), axis=1)
-
-### output my answer
-with open(output_filename, 'w') as fo: 
-    for predict in y_pred_hidden:
-        fo.write(str(predict) + "\n")
-
-### plot_confusion_matrix and calculate precision, recall, f1_score
-#### training set
-y_true = np.argmax(y_train, axis=1)
-y_pred = np.argmax(model.predict(x_train), axis=1)
-#plot_confusion_matrix(y_true, y_pred, classes=np.arange(num_classes),
-#                      title='train_confusion_matrix')
-
-train_precision = precision_score(y_true, y_pred)
-print('train_precision:', train_precision)
-train_recall = recall_score(y_true, y_pred)
-print('train_recall:', train_recall)
-train_f1_score = f1_score(y_true, y_pred)
-print('train_f1_score:', train_f1_score)
-
-#### validation set
-y_true = np.argmax(y_test, axis=1)
-y_pred = np.argmax(model.predict(x_test), axis=1)
-#plot_confusion_matrix(y_true, y_pred, classes=np.arange(num_classes),
-#                      title='validation_confusion_matrix')
-#plt.show()
-
-test_precision = precision_score(y_true, y_pred)
-print('test_precision:', test_precision)
-test_recall = recall_score(y_true, y_pred)
-print('test_recall:', test_recall)
-test_f1_score = f1_score(y_true, y_pred)
-print('test_f1_score:', test_f1_score)
-
-#### average score
-print('average_precision:', (train_precision+test_precision)/2)
-print('average_recall:', (train_recall+test_recall)/2)
-print('average_f1_score:', (train_f1_score+test_f1_score)/2)
-
+    
+    ### decision tree
+    clf = DecisionTreeClassifier()
+    #### training set
+    y_true = np.argmax(y_train, axis=1)
+    clf = clf.fit(x_train, y_true)
+    y_pred = clf.predict(x_train)
+    
+    print('Decision Tree:')
+    train_accuracy = metrics.accuracy_score(y_true, y_pred)
+    print('train_accuracy:', train_accuracy)
+    train_precision = metrics.precision_score(y_true, y_pred)
+    print('train_precision:', train_precision)
+    train_recall = metrics.recall_score(y_true, y_pred)
+    print('train_recall:', train_recall)
+    train_f1_score = metrics.f1_score(y_true, y_pred)
+    print('train_f1_score:', train_f1_score)
+    
+    #### validation set
+    y_true = np.argmax(y_test, axis=1)
+    y_pred = clf.predict(x_test)
+    
+    test_accuracy = metrics.accuracy_score(y_true, y_pred)
+    print('test_accuracy:', test_accuracy)
+    test_precision = metrics.precision_score(y_true, y_pred)
+    print('test_precision:', test_precision)
+    test_recall = metrics.recall_score(y_true, y_pred)
+    print('test_recall:', test_recall)
+    test_f1_score = metrics.f1_score(y_true, y_pred)
+    print('test_f1_score:', test_f1_score)
+    
+    #### average score
+    print('average_accuracy:', (train_accuracy+test_accuracy)/2)
+    print('average_precision:', (train_precision+test_precision)/2)
+    print('average_recall:', (train_recall+test_recall)/2)
+    print('average_f1_score:', (train_f1_score+test_f1_score)/2)
+    
+    ### random forest
+    clf = RandomForestClassifier(n_estimators=1000,
+                                  random_state=0)
+    #### training set
+    y_true = np.argmax(y_train, axis=1)
+    clf = clf.fit(x_train, y_true)
+    y_pred = clf.predict(x_train)
+    
+    print('Random Forest:')
+    train_accuracy = metrics.accuracy_score(y_true, y_pred)
+    print('train_accuracy:', train_accuracy)
+    train_precision = metrics.precision_score(y_true, y_pred)
+    print('train_precision:', train_precision)
+    train_recall = metrics.recall_score(y_true, y_pred)
+    print('train_recall:', train_recall)
+    train_f1_score = metrics.f1_score(y_true, y_pred)
+    print('train_f1_score:', train_f1_score)
+    
+    #### validation set
+    y_true = np.argmax(y_test, axis=1)
+    y_pred = clf.predict(x_test)
+    
+    test_accuracy = metrics.accuracy_score(y_true, y_pred)
+    print('test_accuracy:', test_accuracy)
+    test_precision = metrics.precision_score(y_true, y_pred)
+    print('test_precision:', test_precision)
+    test_recall = metrics.recall_score(y_true, y_pred)
+    print('test_recall:', test_recall)
+    test_f1_score = metrics.f1_score(y_true, y_pred)
+    print('test_f1_score:', test_f1_score)
+    
+    #### average score
+    print('average_accuracy:', (train_accuracy+test_accuracy)/2)
+    print('average_precision:', (train_precision+test_precision)/2)
+    print('average_recall:', (train_recall+test_recall)/2)
+    print('average_f1_score:', (train_f1_score+test_f1_score)/2)
